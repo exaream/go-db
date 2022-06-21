91c774b09199934227f106943288568cc9a6e312
exaream <emctwi8@gmail.com>
2022-06-21 11:09:12 +0900

    [WIP] Refactor basic design

--- example/example.go
+++ example/example.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"io"
-	"os"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -15,15 +13,50 @@ import (
 	"github.com/go-logr/logr"
 )
 
+const (
+	stmtQuery   = `SELECT id, name, status, created_at, updated_at FROM users;`
+	stmtCommand = `UPDATE users SET status = ?, updated_at = NOW() WHERE id = ?;`
+)
+
+type (
+	S any
+	R any
+	T any
+)
 type Executor[S, R, T any] struct {
 	DB     *sql.DB
 	logger *logr.Logger
 }
 
 type Action[S, R, T any] struct {
-	Setup    func(ctx context.Context, tx *sql.Tx) (S, error)
-	Run      func(ctx context.Context, tx *sql.Tx, s S) (R, error)
-	Teardown func(ctx context.Context, db *sql.DB, r R) (T, error)
+	//Setup    func(ctx context.Context, tx *sql.Tx) (S, error)
+	//Run      func(ctx context.Context, tx *sql.Tx, s S) (R, error)
+	//Teardown func(ctx context.Context, db *sql.DB, r R) (T, error)
+}
+
+func Do(ctx context.Context, iniPath, section string) (err error) {
+	db, err := dbx.OpenByIniWithContext(ctx, iniPath, section)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if rerr := db.Close(); err != nil {
+			err = rerr
+		}
+	}()
+
+	var e *Executor[S, R, T]
+	var act *Action[S, R, T]
+	e.DB = db
+
+	_, err = e.Do(ctx, act)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // NOTE: We wrap `do()` with `Do()` to avoid named arguments appering in the document of pkg.go.dev.
@@ -63,27 +96,34 @@ func (e *Executor[S, R, T]) do(ctx context.Context, act *Action[S, R, T]) (zeroT
 	return t, nil
 }
 
+func (act *Action[S, R, T]) Setup(ctx context.Context, tx *sql.Tx) (S, error) {
+	return act.setup(ctx, tx)
+}
+
+func (act *Action[S, R, T]) setup(ctx context.Context, tx *sql.Tx) (zeroS S, err error) {
+	return zeroS, err
+}
+
+func (act *Action[S, R, T]) Run(ctx context.Context, tx *sql.Tx, s S) (R, error) {
+	return act.run(ctx, tx, s)
+}
+
+func (act *Action[S, R, T]) run(ctx context.Context, tx *sql.Tx, s S) (zeroR R, err error) {
+	return zeroR, err
+}
+
+func (act *Action[S, R, T]) Teardown(ctx context.Context, db *sql.DB, r R) (T, error) {
+	return act.teardown(ctx, db, r)
+}
+
+func (act *Action[S, R, T]) teardown(ctx context.Context, db *sql.DB, r R) (zeroT T, err error) {
+	return zeroT, err
+}
+
 // Cond has the fields needed to operate a DB.
 type Cond struct {
-	Writer io.Writer
-	ini    ini
-	stmt   stmt
-	where  where
-	set    set
-}
-type ini struct {
-	path    string
-	section string
-}
-type stmt struct {
-	query   string // Do NOT use the word "select" because it is a reserved word in Go.
-	command string
-}
-type set struct {
-	status int
-}
-type where struct {
 	userId int
+	status int
 }
 
 type user struct {
@@ -95,28 +135,18 @@ type user struct {
 }
 
 // NewCond returns the info needed to operate a DB.
-func NewCond(iniPath, section string, userId, status int) *Cond {
+func NewCond(userId, status int) *Cond {
 	return &Cond{
-		Writer: os.Stdout,
-		ini: ini{
-			path:    iniPath,
-			section: section,
-		},
-		// Please change the following when creating your own package.
-		stmt: stmt{
-			query:   `SELECT id, name, status, created_at, updated_at FROM users;`,
-			command: `UPDATE users SET status = ?, updated_at = NOW() WHERE id = ?;`,
-		},
-		set:   set{status: status},
-		where: where{userId: userId},
+		status: status,
+		userId: userId,
 	}
 }
 
 // Run does a DB operation.
 //TODO: How to shorten this function
-func (c *Cond) Run(ctx context.Context) (rerr error) {
+func (c *Cond) Run(ctx context.Context, iniPath, section string) (rerr error) {
 	// Get DB handle.
-	db, err := dbx.OpenByIni(c.ini.path, c.ini.section)
+	db, err := dbx.OpenByIniWithContext(ctx, iniPath, section)
 	if err != nil {
 		return err
 	}
@@ -127,16 +157,9 @@ func (c *Cond) Run(ctx context.Context) (rerr error) {
 		}
 	}()
 
-	// Check DB connection.
-	if err := db.PingContext(ctx); err != nil {
-		return err
-	}
+	fmt.Println(dbx.LF + "Before operating")
 
-	if err := c.log(dbx.LF + "Before operating"); err != nil {
-		return err
-	}
-
-	records, err := dbx.QueryWithContext(ctx, db, c.stmt.query, scanRows)
+	records, err := dbx.QueryWithContext(ctx, db, stmtQuery, scanRows)
 	if err != nil {
 		return err
 	}
@@ -148,43 +171,33 @@ func (c *Cond) Run(ctx context.Context) (rerr error) {
 		return err
 	}
 
-	// Please add validation here when creating your own package.
-
-	// Please change the following when creating your own package.
-	// TODO: How to abstruct and inject the following arguments.
-	_, err = tx.ExecContext(ctx, c.stmt.command, c.set.status, c.where.userId)
+	_, err = tx.ExecContext(ctx, stmtCommand, c.status, c.userId)
 	if err != nil {
-		return dbx.Rollback(tx, rerr, err)
+		return multierr.Append(rerr, err)
 	}
 
 	// fmt.Println(result.RowsAffected())
-	if err := c.log(dbx.LF + "Before commit"); err != nil {
-		return err
-	}
+	fmt.Println(dbx.LF + "Before commit")
 
-	records, err = dbx.QueryTxWithContext(ctx, tx, c.stmt.query, scanRows)
+	records, err = dbx.QueryTxWithContext(ctx, tx, stmtQuery, scanRows)
 	if err != nil {
 		return err
 	}
 	fmt.Println(records)
 
 	if err := tx.Commit(); err != nil {
-		return dbx.Rollback(tx, rerr, err)
+		return multierr.Append(rerr, err)
 	}
 
-	if err := c.log(dbx.LF + "After operating"); err != nil {
-		return err
-	}
+	fmt.Println(dbx.LF + "After operating")
 
-	records, err = dbx.QueryWithContext(ctx, db, c.stmt.query, scanRows)
+	records, err = dbx.QueryWithContext(ctx, db, stmtQuery, scanRows)
 	if err != nil {
 		return err
 	}
 	fmt.Println(records)
 
-	if err := c.log(""); err != nil {
-		return err
-	}
+	fmt.Println("")
 
 	return nil
 }
@@ -227,6 +240,6 @@ func scanRows(ctx context.Context, rows *sql.Rows) (_ dbx.Records, err error) {
 }
 
 func (c *Cond) log(msg string) error {
-	_, err := fmt.Fprintln(c.Writer, msg)
+	_, err := fmt.Println(msg)
 	return err
 }
